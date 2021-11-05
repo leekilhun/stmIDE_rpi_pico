a5df03b47184af8af16f4b0be284839cbd7a4ba9
leekilhun <pba7189@gmail.com>
2021-11-06 08:07:15 +0900

    led blink

---
+++ pico_fw/src/bsp/pico/config_autogen.h
@@ -0,0 +1,13 @@
+/*
+ * config_autogen.h
+ *
+ *  Created on: 2021. 11. 4.
+ *      Author: gns2l
+ */
+
+#ifndef SRC_BSP_PICO_CONFIG_AUTOGEN_H_
+#define SRC_BSP_PICO_CONFIG_AUTOGEN_H_
+
+#include "boards/pico.h"
+
+#endif /* SRC_BSP_PICO_CONFIG_AUTOGEN_H_ */
---
+++ pico_fw/src/bsp/pico/version.h
@@ -0,0 +1,16 @@
+/*
+ * version.h
+ *
+ *  Created on: 2021. 11. 4.
+ *      Author: gns2l
+ */
+
+#ifndef SRC_BSP_PICO_VERSION_H_
+#define SRC_BSP_PICO_VERSION_H_
+
+#define PICO_SDK_VERSION_MAJOR 1
+#define PICO_SDK_VERSION_MINOR 2
+#define PICO_SDK_VERSION_REVISION 0
+#define PICO_SDK_VERSION_STRING "${PICO_SDK_VERSION_STRING}"
+
+#endif /* SRC_BSP_PICO_VERSION_H_ */
